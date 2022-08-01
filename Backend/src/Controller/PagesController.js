f3f4e71ae464d93fc489f3b9eba05729729bc62d
Burger-23 <gabrielfrancisco2004@gmail.com>
2022-08-01 10:47:30 -0300

    Sei la

--- Backend/src/Controller/PagesController.js
+++ Backend/src/Controller/PagesController.js
@@ -4,10 +4,10 @@ module.exports = class PagesController{
         res.render('feed');
     }
     static login(req, res){    
-        res.render('login', {});
+        res.render('login', {layout: ''});
     }
     static registro(req, res){    
-        res.render('registro', {});
+        res.render('registro', {layout: ''});
     }
 
 }
